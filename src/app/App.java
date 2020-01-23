2eea7e642b5d490b37965e4517b1490479a58eee
Visar1 <salihi.visar@gmail.com>
2020-01-23 20:10:01 +0100

    loc test

--- src/app/App.java
+++ src/app/App.java
@@ -5,5 +5,9 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("hello world");
+
+        Person personLoc = new Person();
+        personLoc.setName("Loc");        
+        System.out.println("hello " + personLoc.getName());
     }
 }

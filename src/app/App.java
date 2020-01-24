db4fb2930e7e837e9f20cac49927a565073d74b1
Visar1 <salihi.visar@gmail.com>
2020-01-24 12:10:37 +0100

    update 24.1.20 12.00

--- src/app/App.java
+++ src/app/App.java
@@ -18,14 +18,28 @@ public class App {
         personVisar.setName("Visar");
         System.out.println("hello " + personVisar.getName());
     
+        
         System.out.println( personLoc.getName() + " hat " + personLoc.getFinger()+" Finger.");
+    
+        if(personLoc.getFinger()>= 10){
+            System.out.println("Dann Finger brechen!");
+        }
+        else{
+            System.out.println("Bitte keine Finger brechen!");
+        }
+       
+        if(personLoc.getGebrochenefinger()>personLoc.getFinger()){
+            System.out.println("Dann kannst du Finger brechen!");
+        }
+
+       
         
         personLoc.breche_finger(4);
         System.out.println(personVisar.getName()+ " bricht am " + personLoc.getName()+" " + personLoc.getGebrochenefinger()+" Finger.");
         
         System.out.println(personLoc.getName()+" hat noch "+personLoc.getFinger()+" Finger übrig.");
-
         
+     
 
 
         System.out.println(personVisar.getName() + " hat auch "+ personVisar.getFinger()+ " Finger.");
